c31c2c6b333a6ea67a45cc5004bbbf33a8295e4f
int_szyk <szymon.kwiatkowski@mobica.com>
2019-09-19 15:57:01 +0200

    fragmentation of code

---
+++ Brewing.cpp
@@ -0,0 +1,188 @@
+#include "Brewing.h"
+
+int readKeypad(LiquidCrystal lcd, Keypad kpd)
+{
+    char key;
+    bool finished = false;
+    int ret = 0;
+
+    lcd.setCursor(0, 1);
+    lcd.print(String(ret));
+    lcd.blink();
+
+    while (!finished) {
+        key = kpd.waitForKey();
+        switch (key) {
+            case '1':
+                ret = ret * 10 + 1;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '2':
+                ret = ret * 10 + 2;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '3':
+                ret = ret * 10 + 3;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '4':
+                ret = ret * 10 + 4;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '5':
+                ret = ret * 10 + 5;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '6':
+                ret = ret * 10 + 6;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '7':
+                ret = ret * 10 + 7;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '8':
+                ret = ret * 10 + 8;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '9':
+                ret = ret * 10 + 9;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case '0':
+                ret = ret * 10 + 0;
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            case 'A':
+                finished = true;
+                break;
+
+            case 'B':
+                ret = 0;
+                finished = true;
+                break;
+
+            case 'C':
+                ret = ret / 10;
+
+                lcd.setCursor(0, 1);
+                lcd.print("                ");
+
+                lcd.setCursor(0, 1);
+                lcd.print(String(ret));
+                lcd.blink();
+
+                delay(20);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // Serial.print(ret); // Debug
+    return ret;
+}
+
+float temperature(OneWire thermometer)
+{
+    byte data[12];
+    byte addr[8];
+
+    do {
+        thermometer.reset_search();
+    } while (!thermometer.search(addr));
+
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!"); // Debug
+    }
+
+    thermometer.reset();
+    thermometer.select(addr);
+    thermometer.write(0x44, 1);        // start conversion, with parasite power on at the end
+
+    delay(100); //!!!! needed?
+    // we might do a thermometer.depower() here, but the reset will take care of it.
+
+    thermometer.reset();
+    thermometer.select(addr);
+    thermometer.write(0xBE);         // Read Scratchpad
+
+    for (int i = 0; i < 9; ++i) {           // we need 9 bytes
+        data[i] = thermometer.read();
+    }
+
+    int16_t raw = (data[1] << 8) | data[0];
+
+    delay(200); //!!!! needed?
+
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) {
+        raw = raw & ~7;    // 9 bit resolution, 93.75 ms
+    }
+
+    delay(200); //!!!! needed?
+
+    return (float)raw / 16.0;
+}
---
+++ Brewing.h
@@ -0,0 +1,16 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+#include <OneWire.h>
+
+/*
+ * Function used to read value from keypad and display it on the LCD.
+ * Press A to accept value.
+ * Press B to cancel entering value.
+ * Press C to delete last character.
+ */
+int readKeypad(LiquidCrystal lcd, Keypad kpd);
+
+/*
+ * Function returns temperature in celsius degrees as float.
+ */
+float temperature(OneWire thermometer);
